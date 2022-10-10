2dc6090d4fcd49faa290629c5117b6f2a9e71bf1
Denis Johnson <G00403126@atu.ie>
2022-10-10 11:21:19 +0100

    added divide

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,25 @@
 package ie.atu;
+import java.util.Scanner;
 
-public class Calc {
-    public static void main(String[] args) {
-// new project
+public class Calc
+{
+
+    public static void main(String[] args)
+    {
+     divide();
+    }
+    public static void divide()
+    {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number ");
+        int firstNumber = input.nextInt();
+        System.out.println("You entered : "+ firstNumber);
+        System.out.println("Please enter a second number ");
+        int secondNumber = input.nextInt();
+        System.out.println("You entered : "+ secondNumber);
+        int total = firstNumber / secondNumber;
+        System.out.println("The two numbers divided is : "+ total);
+
     }
 }
