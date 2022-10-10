37d273d2868986312bbfd9342509884cd6480637
praise ojofeitimi <g00407342>
2022-10-10 10:47:06 +0100

    added commit

---
+++ src/main/java/ie/atu/Calc.java
@@ -0,0 +1,27 @@
+package ie.atu;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class calc2022
+{
+    public static void main(String[] args);
+    {
+        subtract();
+    }
+
+    public static void subtract()
+    {
+        System.out.println("Please enter your first number");
+        Scanner input = new Scanner(System.in);
+        int firstNumber = input.nextInt();
+
+        System.out.println("Please enter second Number");
+        int SecondNumber = input.nextInt();
+        int total = firstNumber - SecondNumber;
+        System.out.println("The total is " + total);
+    }
+
+
+
+}
