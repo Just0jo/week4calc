03973d3526060fed06cc8a4d078eb17bfab1491c
Jakub Mamala <G00408184@atu.ie>
2022-10-10 11:16:59 +0100

    multiple feature

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,24 @@
 package ie.atu;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Calc {
-    public static void main(String[] args) {
-// new project
+    public static void main(String[] args){
+        multiply();
     }
+
+
+
+    public static void multiply() {
+        System.out.println("Please enter your first number");
+        Scanner input = new Scanner(System.in);
+        int firstNumber = input.nextInt();
+
+        System.out.println("Please enter second Number");
+        int SecondNumber = input.nextInt();
+        int total = firstNumber * SecondNumber;
+        System.out.println("The total is " + total);
+    }
+
+
 }
